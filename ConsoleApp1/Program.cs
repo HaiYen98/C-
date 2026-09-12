20466edcb31bab15f0395b232c7ed5a41a83aae8
HaiYenn <mbhyen14092006@gmail.com>
2026-09-12 11:49:05 +0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,58 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+int a;
+int b;
+int c;
+
+Console.Write("Nhập a: ");
+
+while (true)
+{
+    try
+    {
+        a = int.Parse(Console.ReadLine()!);
+        break;
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nhập sai, vui lòng nhập lại!");
+        Console.Write("Nhập a: ");
+    }
+}
+
+
+while (true)
+{
+    Console.Write("Nhập b: ");
+
+    try
+    {
+        b = int.Parse(Console.ReadLine()!);
+        break;
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nhập sai, vui lòng nhập lại!");
+    }
+}
+
+while (true)
+{
+    Console.Write("Nhập c: ");
+
+    try
+    {
+        c = int.Parse(Console.ReadLine()!);
+        break;
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nhập sai, vui lòng nhập lại!");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("KẾT QUẢ");
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"c = {c}");
