f8cb492c7b6603bfad06dbcca14e8f4f423b56a5
HaiYenn <mbhyen14092006@gmail.com>
2026-09-16 19:06:22 +0700

    Add files via upload

---
+++ HinhTron/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        double banKinh = 0;
+
+        Console.Write("Nhập vào bán kính hình tròn (m): ");
+        banKinh = double.Parse(Console.ReadLine()!);
+
+        double chuVi = 2 * Math.PI * banKinh;
+        double dienTich = Math.PI * banKinh * banKinh;
+
+        Console.WriteLine("Chu vi hình tròn là: {0}", chuVi);
+        Console.WriteLine("Diện tích hình tròn là: {0}", dienTich);
+
+        Console.WriteLine("Nhập phím bất kỳ để thoát");
+        Console.ReadKey();
+    }
+}
